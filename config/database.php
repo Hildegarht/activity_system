2cc80334f96fffc28faece318d02ea6f5c55cf49
Hildegarht <hildaamadu@gmail.com>
2022-05-28 08:14:07 +0000

    adding postgres configuration

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'postgres'),
 
     /*
     |--------------------------------------------------------------------------
@@ -63,14 +64,29 @@ return [
             ]) : [],
         ],
 
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'search_path' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'postgres://ibnptfadkbocrn:eb11cfe51696e408c6e1a2a2356afbe978d442423a8ba33beed10b29b51be8f3@ec2-3-234-131-8.compute-1.amazonaws.com:5432/d1a192gfaf1ffo'),
+            'host' => env('DB_HOST', 'ec2-3-234-131-8.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd1a192gfaf1ffo'),
+            'username' => env('DB_USERNAME', 'ibnptfadkbocrn'),
+            'password' => env('DB_PASSWORD', 'eb11cfe51696e408c6e1a2a2356afbe978d442423a8ba33beed10b29b51be8f3'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
